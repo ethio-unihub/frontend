4502f26d38ebcb4445ec31d34e8829da4ab44650
Leulseged Melaku <132431582+Ludis-ET@users.noreply.github.com>
2024-03-28 19:11:35 +0300

    creating pages

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import { ALLRoutes } from "./routes/ALLRoutes";
+import { Header } from "./components";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <ALLRoutes />
+      {/* <Footer/> */}
+    </>
+  );
+}
+
+export default App;
