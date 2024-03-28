25dca14450e5388f8b011ae10a4b282d58629bff
Leulseged Melaku <132431582+Ludis-ET@users.noreply.github.com>
2024-03-28 19:08:17 +0300

    clear the repo

--- src/components/index.js
+++
@@ -1,3 +0,0 @@
-export { Header } from './Header'
-export { LeftBar } from './LeftBar'
-export { FeedCard } from './FeedCard'
--- src/pages/index.js
+++
@@ -1,3 +0,0 @@
-export {Home} from './Home/Home'
-export {Login} from './Authentication/Login'
-export {Register} from './Authentication/Register'
