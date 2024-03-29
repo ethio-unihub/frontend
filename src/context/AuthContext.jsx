2a745d98549f89766af1c439386e10b54c7b5df9
Ludis-et <leulsegedmelaku1020@gmail.com>
2024-03-29 09:37:22 +0300

    somechanges

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,4 +1,5 @@
 import { createContext, useState, useEffect } from "react";
+const backendUrl = import.meta.env.VITE_REACT_APP_BACKEND_URL;
 
 export const AuthContext = createContext();
 
@@ -7,9 +8,26 @@ export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
 
   let loginUser = async (e) => {
-    let response = await fetch()
+    let response = await fetch(`${backendUrl}auth/jwt/create`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username: e.target.username.value,
+        password: e.target.password.value,
+      }),
+    });
+    let data = response.json();
+    if (response.status === 200) {
+      setauthTokens(data);
+    } else {
+      alert("Invalid credentials");
+    }
   };
 
-  const value = {};
+  const value = {
+    loginUser: loginUser,
+  };
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 };
--- src/pages/Authentication/Login.jsx
+++ src/pages/Authentication/Login.jsx
@@ -1,8 +1,10 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { Input, Ripple, initTWE } from "tw-elements";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../../context/AuthContext";
 
 export function Login() {
+  let { loginUser } = useContext(AuthContext);
   useEffect(() => {
     initTWE({ Input, Ripple });
   }, []);
@@ -17,7 +19,8 @@ export function Login() {
   const onSubmit = (e) => {
     e.preventDefault();
 
-    //login
+    loginUser(e);
+    setFormData({ username: "", password: "" })
   };
 
   return (
@@ -44,11 +47,12 @@ export function Login() {
                 <input
                   type="text"
                   id="username"
+                  autoComplete="off"
                   className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                   placeholder="abebe"
                   name="username"
                   value={username}
-                  onChange={e => onChange(e)}
+                  onChange={(e) => onChange(e)}
                   required
                 />
               </div>
@@ -62,7 +66,7 @@ export function Login() {
                 <input
                   type="password"
                   id="password"
-                  onChange={e => onChange(e)}
+                  onChange={(e) => onChange(e)}
                   className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                   name="password"
                   value={password}
