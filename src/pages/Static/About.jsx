cf68e329b9f53424070a0c30e607fc5bfc347f76
samual keleb <samualkelebu@gmail.com>
2024-04-14 00:38:46 +0300

    styling

--- src/pages/Static/About.jsx
+++ src/pages/Static/About.jsx
@@ -7,16 +7,16 @@ export const About = () => {
       {/* <h1>abebe</h1> */}
       <main className="flex-col font-mono dark:text-white">
         <h1 className="flex mx-auto justify-center text-3xl ">About us </h1>
-        <p>
+        <p className="flex mx-auto justify-center mt-10">
           Team Alpha Students at Addis Ababa Sciense And Technology University{" "}
-          <span> GDSC AASTU </span>
+          <span className="text-yellow-600"> GDSC AASTU </span>
         </p>
       </main>
-      <section>
-        <div className="flex mx-auto dark:text-white mt-[10vh] rounded-lg overflow-hidden">
+      <section >
+        <div className="flex mx-auto justify-center dark:text-white mt-[10vh] rounded-lg overflow-hidden">
           <div className="flex flex-col w-[30vh] h-[10vh] shadow-lg">
             <h1>Lemisa Elias</h1>
-            <img src="" alt="" />
+            <img src="image.png" alt="" />
             <p>Front End Lead</p>
           </div>
           <div className="flex flex-col w-[30vh] h-[10vh] shadow-lg">
@@ -30,7 +30,7 @@ export const About = () => {
             <p>Back End Dev</p>
           </div>
         </div>
-        <div className="flex mx-auto dark:text-white h-[10vh]">
+        <div className="flex mx-auto justify-center dark:text-white h-[10vh]">
           <div className="flex flex-col w-[30vh]">
             <h1>Samual Keleb</h1>
             <img src="" alt="" />
@@ -47,7 +47,7 @@ export const About = () => {
             <p>Back End Dev</p>
           </div>
         </div>
-        <div className="flex mx-auto dark:text-white">
+        <div className="flex mx-auto justify-center dark:text-white">
           <div className="flex flex-col w-[30vh]">
             <h1>Michael Workineh</h1>
             <img src="" alt="" />
